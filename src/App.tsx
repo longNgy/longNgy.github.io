56a0c8f38c6bf880087fe70af06429a95fa76b32
Long Nguyen <93321767+KuroiKaze@users.noreply.github.com>
2026-07-25 01:09:23 +0200

    add: react portfolio

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <h1>Long Nguyen</h1>
+      <p>Informatik Student an der Hochschule Darmstadt</p>
+      <p>App-Entwickler</p>
+    </main>
+  )
+}
+
+export default App
